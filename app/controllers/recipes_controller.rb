ca2032af4181f6d1626ee61b53569d621f1b1137
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-30 19:32:47 -0400

    commit tree 4618f543f225414be4cb81c1214bf8b53cc655a8

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }#(name_type: 'name')
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    end
+  end
+
+  private
+
+  def recipe_params
+
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [ :name, :quantity ]
+    )
+   
+
   end
 end
